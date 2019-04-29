65033d6826b74bc041e62ea1e81d5055c6e77896
joeco fong <joecofg@gmail.com>
2019-04-28 23:36:11 -0700

    added a different game taken from some tutorial and added it to our homepage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 const express = require('express');
-const loginRoutes = require('./routes/admin');
+const loginRoutes = require('./routes/login');
 const gameRoutes = require('./routes/game');
 
 

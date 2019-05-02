54b488c7cef61838c30623276bd920db0b15b3ec
joeco fong <joecofg@gmail.com>
2019-05-01 20:34:31 -0700

    added the react front end

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const leaderboardRoutes = require('./routes/leaderboard');
 const settingsRoutes = require('./routes/settings');
 const profileRoutes = require('./routes/profile');
 
-
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/public'));
+
 app.use(loginRoutes);
 app.use(gameRoutes);
 app.use(menuRoutes);
@@ -23,11 +23,21 @@ app.use(leaderboardRoutes);
 app.use(settingsRoutes);
 app.use(profileRoutes);
 
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('*', (req, res) => {
+//     res.sendfile(path.join(__dirname = 'client/build/index.html'));
+//   })
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 app.use((req,res) => {
   res.status(404).sendFile(path.join(__dirname, '/public/html', '404.html'));
 })
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
 });
 

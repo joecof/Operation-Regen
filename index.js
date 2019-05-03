11280b3491118f420af380bc0b5e1a78408acc33
joeco fong <joecofg@gmail.com>
2019-05-03 11:45:42 -0700

    added client side routing

--- index.js
+++ index.js
@@ -2,33 +2,39 @@ var path = require('path');
 
 const express = require('express');
 
-const loginRoutes = require('./routes/login');
-const gameRoutes = require('./routes/game');
-const menuRoutes = require('./routes/menu');
-const leaderboardRoutes = require('./routes/leaderboard');
-const settingsRoutes = require('./routes/settings');
-const profileRoutes = require('./routes/profile');
+// const loginRoutes = require('./routes/login');
+// const gameRoutes = require('./routes/game');
+// const menuRoutes = require('./routes/menu');
+// const leaderboardRoutes = require('./routes/leaderboard');
+// const settingsRoutes = require('./routes/settings');
+// const profileRoutes = require('./routes/profile');
 
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.use(loginRoutes);
-app.use(gameRoutes);
-app.use(menuRoutes);
-app.use(leaderboardRoutes);
-app.use(settingsRoutes);
-app.use(profileRoutes);
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`${req.body.post}`,);
+})
+
+// app.use(loginRoutes);
+// app.use(gameRoutes);
+// app.use(menuRoutes);
+// app.use(leaderboardRoutes);
+// app.use(settingsRoutes);
+// app.use(profileRoutes);
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-//   app.get('*', (req, res) => {
-//     res.sendfile(path.join(__dirname = 'client/build/index.html'));
-//   })
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
 
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 

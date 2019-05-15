36d0ddc7bc8bef1b9c848fe10ef0d4569dd23bc2
joeco fong <joecofg@gmail.com>
2019-05-15 13:11:39 -0700

    changed the background image

---
+++ client/src/js/components/Transition.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import '../../css/Transition.css';
+import '../../img/transitionbase0.png';
+
+
+
+export default class Transition extends Component {
+  render() {
+    return (
+      <div className = "Transition">
+
+
+        {/* <img className = "Transition" src={require('../../img/logo.png')} alt = "Operation Regen"/> */}
+
+      </div>
+    )
+  }
+}

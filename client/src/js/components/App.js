d7062b5c7f5a16426659b91e2fb3b31a58f1f0e0
joeco fong <joecofg@gmail.com>
2019-05-16 09:57:04 -0700

    minor changes

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -7,18 +7,52 @@ import LeaderBoardBox from './LeaderBoardBox';
 import GameContainer from './GameContainer';
 import Credits from './Credits';
 import Transition from './Transition';
+import bg from "../../img/transitionbase9.png"
+
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      BackgroundHead: {
+        backgroundImage: `url(${bg})`
+        // backgroundSize: 'cover',
+        // backgroundRepeat: 'no-repeat',
+        // backgroundAttachment: 'fixeds'
+      }
+    }
+
+  }
+
+
+    handler() {
+      this.setState({
+        BackgroundHead: {
+          backgroundImage: `url(${bg})`
+        }
+      })
+    };
+
+    
+
+
+
 
   render() {
 
     return (
-      <div className="App">
+      <div className="App" style = {this.state.BackgroundHead}>
         <Switch>
           <Route exact path = '/' component = {PlayBoxContainer} />
           <Route exact path = '/LeaderBoard' component = {LeaderBoardBox} />
           {/* <Route exact path = '/Play' component = {GameContainer} /> */}
-          <Route exact path = '/Play' component = {Transition} />
+          <Route exact path = '/Play' component = {Transition} 
+            
+          />
           <Route exact path = '/' component = {Credits} />
           <Route render = {() =>  <h1> 404 Not Found </h1>}/>
         </Switch>  
@@ -27,5 +61,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
--- client/src/js/components/Transition.js
+++ client/src/js/components/Transition.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
 
 import '../../css/Transition.css';
-import '../../img/transitionbase0.png';
-
-
+import "../../img/transitionbase0.png"
 
 export default class Transition extends Component {
+
+  constructor(props) {
+    super(props);
+
+
+
+  }
+  
+
+  
   render() {
+
+    // const BackgroundHead = {
+    //   backgroundImage: `url(${"../../img/transitionbase0.png"})`,
+    //   backgroundSize: 'cover',
+    //   backgroundRepeat: 'no-repeat',
+    //   backgroundAttachment: 'fixeds'
+    //   }
+      
+
+
     return (
-      <div className = "Transition">
+      <div className = "Transition" >
 
+        <button className = "button" onClick = {this.props.changeBg("../../img/transitionbase0.png")}> Click me </button>
+      
 
-        {/* <img className = "Transition" src={require('../../img/logo.png')} alt = "Operation Regen"/> */}
+        {/* <img className = "Transition" src={require('../../img/transitionbase0.png')} alt = "Operation Regen"/> */}
+
 
       </div>
     )

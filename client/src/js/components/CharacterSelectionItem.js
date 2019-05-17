46174789d2bfca79268f0d21929ee9bbb12fd2ea
HO JOO LEE <hojoolee81@gmail.com>
2019-05-16 23:31:13 -0700

    alter hero selection component, improve leaderboard component rendering speed

--- client/src/js/components/CharacterSelectionItem.js
+++ client/src/js/components/CharacterSelectionItem.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react'
-import '../../css/CharacterSelectionItem.css'
+import React, { Component } from 'react';
+import '../../css/CharacterSelectionItem.css';
 
 class CharacterSelectionItem extends Component {
-  render() {
-    const clicked = () => {
-      //alert("clicked!");
-      alert(this.props.hero);
+  state = {
+    aniIndex: 6
+  }
+
+  constructor(props) {
+    super(props);
+    var count = this.state.aniIndex;
+    this.backgrounds = new Array(++count);
+
+    for (let i = 0; i < count; i++) {
+      this.backgrounds[i] = require('../../img/hero' + this.props.hero + 'ani' + i + '.png');
     }
 
+    this.state = { imageIndex: 0 };
+    this.toggleAnimate = this.toggleAnimate.bind(this);
+  }
+
+  //componentDidMount () {
+  //  this.timeout = setTimeout(this.toggleAnimate, this.props.animDuration * 100);
+  //}
+  
+  //componentWillUnmount() {
+  // 	if (this.timeout) {
+  //    clearTimeout(this.timeout);
+  //  }
+  //}
+
+  toggleAnimate() {
+    if (this.state.imageIndex !== 6) {  //this.stats.aniIndex
+      this.setState(({imageIndex}) => {
+        return { imageIndex: ++imageIndex };
+      }, () => {
+        this.timeout = setTimeout(
+          this.toggleAnimate,
+          this.props.animDuration * 100
+        )
+      })
+    } else {
+
+    }
+  }
+
+  render() {
     return (
-        <img className = "CharacterSelectionItem-Img" src = {require('../../img/hero' + this.props.hero + 'H.png')} alt = "hero" onClick = {() => clicked()} />
+        <img className = "CharacterSelectionItem-Img" src = {this.backgrounds[this.state.imageIndex]}
+          alt = "hero" onClick = {this.toggleAnimate} />
     )
   }
 }

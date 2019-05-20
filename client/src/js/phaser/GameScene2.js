44ccb13834e79d85732485dfd1484d99d8ab1616
joeco fong <joecofg@gmail.com>
2019-05-19 17:37:36 -0700

    deleted transition and title scene cuz not necessary

--- client/src/js/phaser/GameScene2.js
+++ client/src/js/phaser/GameScene2.js
@@ -1,125 +1,169 @@
 import * as Phaser from 'phaser'
 
-var instr   = false;    
-var win     = false;
-var lose    = false;
-var gameWidth   = window.innerWidth;
-var gameHeight  = window.innerHeight;  
-var angle       = 0;
-var speed       = gameWidth/200;
-var randNum     = Math.random() * 1700 + 100;
+  var gameWidth   = window.innerWidth;
+  var gameHeight  = window.innerHeight;
+  var scaleY = gameHeight / 1080;
+  var scaleX = gameWidth / 1776;       
+  var bkgr;    
+  var truck;
+  var instruction;
+  var garbage;
 
-var bulldozer;
-var bg;
-var instruction;
-var tree;
+  var instr   = true;
+  var drag    = false;
+  var drop    = false;
+  var win     = false;
+  var lose    = false;
+  var back    = false;
+  var mult    = 1;
+  var LOOP    = 2;
+  var angle   = 0;
+  var target  = 5;
 
-export default class GameScene2 extends Phaser.Scene {
+  var initX   = -100;
+  var speed   = 5 + (Math.random() * 5);
+  var speedY  = 0;
+  var height  = gameHeight/1.15;
 
-  constructor(){
-    super({key: "GameScene2"})
+
+export default class GameScene3 extends Phaser.Scene {
+
+  constructor() {
+    super({ key: 'GameScene3'})
   }
 
-  preload(){
-
-    this.load.image('background', '../img/bkgr.png');
-    this.load.image('tree', '../img/tree.png');
-    this.load.image('bull', '../img/enemy.png');
-    instruction = this.load.image('instruction', '../img/Instruction.png');
-    
-  }
-
-  drawBull(){
-    if(bulldozer.x < gameWidth && bulldozer.x < tree.x){
-        bulldozer.x += speed;
-        angle += 3;
-        bulldozer.y += (1.25*Math.cos(15 * angle));
-    } else if(bulldozer.x >= tree.x){
-        angle   = 0;
-        lose    = true;
-    } else if(bulldozer.x >= gameWidth){
-        bulldozer.x = -200;
-    }
-  }
-
-  showInstruction(){
-    var slowS 	= (gameWidth - 690)/3;
-    var slowE 	= slowS + 250;
-    var trigger = gameWidth + randNum;
-    var speed2	= speed * 1.75;
-
-    if(instruction.x < slowS){ 
-        instruction.x += speed2;     
-    } else if (instruction.x >= slowS && instruction.x < slowE){
-        instruction.x += 5;
-    } else if (instruction.x >= slowE && instruction.x < trigger){
-        instruction.x += speed2;     
-    } else if (instruction.x >= trigger){
-        instr = true;
-    }
-  }
-  
-  victoryLoop(){
-    if(bulldozer.x < gameWidth * 2){                     
-        angle += 0.25;
-        bulldozer.angle += 30;
-        bulldozer.x += 10;
-        bulldozer.y += (80 * Math.cos(0.1 * angle + 92) + 10);
-    } else {
-    }
-
-  }
-
- loserLoop(){
-    if(tree.angle <= 450){
-        tree.angle += 5;
-        tree.x += 26;
-        tree.y += (50 * Math.cos(0.1 * angle + 42) + 10);
-    } else {
-
-    }
+  preload() {
+    this.load.spritesheet('background', '../img/riverSheet.png',{ frameWidth: 1776, frameHeight: 1080 });
+    this.load.spritesheet('backgroundLoss', '../img/riverSheetLoss.png',{ frameWidth: 1776, frameHeight: 1080 });
+    this.load.image('truck', '../img/Truck.png');
+    this.load.image('garbage', '../img/trash.png');
+    this.load.image('garbageP', '../img/trashPicked.png');
+    this.load.image('instruction', '../img/Instruction.png');
   }
 
   create() {
 
-    bg          = this.add.image(850, 500, 'background');
-    tree        = this.add.image(1200, gameHeight/2 + 80, 'tree');
-    bulldozer   = this.add.image(-300, gameHeight/2 + 120, 'bull').setInteractive();
-    instruction = this.add.image(-700, gameHeight/2 - 250, 'instruction');        
+    var config  = {
+      key: 'flow',
+      frames: this.anims.generateFrameNumbers('background'),
+      frameRate: 24,
+      repeat: -1
+    };
 
-    var scaleY = gameHeight / 1080;
-    var scaleX = gameWidth / 1776;
-    bg.scaleY  = scaleY;
-    bg.scaleX  = scaleX;
-    bulldozer.setScale(2.5);
+    this.anims.create(config);
 
-    bulldozer.on('pointover', () => {
-    })
+    bkgr  = this.add.sprite(0, 0, 'background').setOrigin(0,0);
+    bkgr.scaleY  = scaleY;
+    bkgr.scaleX  = scaleX;
+    bkgr.anims.load('flow');
+    bkgr.anims.play('flow');
 
-    bulldozer.on('pointerout', () => {
-    })
-    
-    bulldozer.on('pointerdown', function (pointer) {
-        console.log(lose);
-        if(lose === false){
-            angle = 0;
-            win = true;
-        } else {
-          
-        }            
+    truck = this.add.image(gameWidth/7,gameHeight/1.65, 'truck');
+    truck.setScale(1.5);
+
+    instruction = this.add.image(gameWidth/2,(gameHeight - 45)/2, 'instruction');
+
+    garbage = this.add.image(initX, gameHeight/1.15, 'garbage').setInteractive();        
+    garbage.setScale(1.25);
+    this.input.setDraggable(garbage);
+
+    this.input.on('dragstart', function (pointer, gameObject) {
+        garbage.setTexture('garbageP');
+        drag = true;
     });
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        if(gameObject.x < truck.x + 192 && gameObject.x > truck.x - 192
+        && gameObject.y < truck.y + 62 && gameObject.y > truck.y - 62){
+            truck.setScale(2);
+        } else{
+            truck.setScale(1.5);
+        }
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+    });
+
+    this.input.on('dragend', function (pointer, gameObject) {
+        if(gameObject.x < truck.x + 192 && gameObject.x > truck.x - 192
+        && gameObject.y < truck.y + 62 && gameObject.y > truck.y - 62){
+            truck.setScale(1.5);
+            target--;
+            if(target < 0){
+                win = true;
+            }                
+
+            var rand    = gameHeight/1.15 + (Math.random()*40 + 10);
+            height      = rand;
+            garbage.x   = initX;
+            garbage.y   = height;
+            speed       = 5 + (Math.random() * 5);                
+        }
+        speedY  = 15;
+        drag    = false;
+    });
+
   }
 
-  update (){     
-    if(instr === false && win === false && lose === false){
+  update() {
+
+    if(instr === true){
         this.showInstruction();
-    } else if(instr === true && win === false && lose === false){				
-        this.drawBull();				
+    } else if(drag === false && win === false && lose === false){            
+        this.throwTrash();
     } else if(win === true){
-        this.victoryLoop();
-    } else if(lose === true){
-      // export let wincondition = true;
-        this.loserLoop();
+        if(truck.x > -250){
+            truck.x -= 5;
+            angle   += 3;
+            truck.y += (1.25*Math.cos(15 * angle));
+        } else {
+
+            //Transition here
+        }            
+    } else if(lose == true){
+        bkgr.setTexture('backgroundLoss');
+        //Transition here
     }
   }
-} 
+
+  showInstruction(){
+    var speed = 0.04
+    var max = 2;
+    var min = 1;
+    if(instr === true){
+        if(mult < max && back === false){                
+            instruction.setScale(mult += speed);
+            if(mult >= max){
+                back = true;                                        
+            }                
+        }
+        if(mult > min && back === true){
+            instruction.setScale(mult -= speed);
+            if(mult <= min){
+                back = false;
+                LOOP--;
+            }
+        }
+        if(LOOP < 0){
+            instr = false;
+            instruction.x = -1000;
+        }
+    }
+  }
+
+  throwTrash(){
+    if(garbage.y < height){
+        garbage.x -= speed;
+        garbage.y += speedY;
+    } else {
+        garbage.setTexture('garbage');
+        speedY = 0;
+    }
+    if(garbage.x < gameWidth + 30){            
+        garbage.x += speed;            
+    } else{            
+        garbage.x = initX;
+        garbage.y = height;
+        lose = true;
+    }
+  }
+}

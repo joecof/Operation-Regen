5da50d304fff2973467b6b80bd95ad98c4671d7d
HO JOO LEE <hojoolee81@gmail.com>
2019-05-21 18:54:35 -0700

    alter play box

--- client/src/js/components/PlayBox.js
+++ client/src/js/components/PlayBox.js
@@ -28,9 +28,11 @@ class PlayBox extends Component {
   enterHeroName(name) {
     this.setState({heroName: name, nameChange: true});
   }
+
   info1 = () => {
     message.info('Select your hero!');
   };
+
   info2 = () => {
     message.info('Enter your hero name!');
   };
@@ -40,10 +42,8 @@ class PlayBox extends Component {
   checkUserInput() {
     if (this.state.selectedHero === null) {
       this.info1();
-      // alert("Select your hero!");
       return "";
     } else if (this.state.heroName === null) {
-      // alert("Enter your hero name!");
       this.info2();
       this.setState({nameChange: true});
       return "";

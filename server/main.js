ac6dfcc53cbb157475b9d7d947c9fb5a83826eac
joeco fong <joecofg@gmail.com>
2019-05-23 02:51:23 -0700

    make it better

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mysql = require('mysql');
-const port = 3001;
+let port = process.env.PORT || 3001;
 
 // includes db configuration info
 let dbconfig = require(__dirname + '/config/db-config.json');

a714a5d98895b594f0715db8c7b046964bec77b9
HO JOO LEE <hojoolee81@gmail.com>
2019-05-23 02:06:22 -0700

    test

--- client/src/js/components/Credits.js
+++ client/src/js/components/Credits.js
@@ -15,7 +15,7 @@ export default class Credits extends Component {
         <img className ="kid" src = {require("../../img/gamekid.png")}/>
         <p className="desc">Our idea for Operation Regen came from a desire for both children’s entertainment to have good, positive messages, and for children’s education to be fun and interactive. 
           Throughout each of our individual lives, the memories and messages that have stuck with us the most have been the times when we have had the most fun. Because of this, we feel that it’s important to get young children invested in entertainment with eco-friendly messages to hopefully impact them later in life. 
-          The people we are attempting to target with Operation Reject will be the voters, electors, and decision-makers of the future.</p>
+          The people we are attempting to target with Operation Regen will be the voters, electors, and decision-makers of the future.</p>
 
         <p className="desc">Operation Regen combines the microgame-style gameplay of titles such as WarioWare with the theming and positive messages of television series such as Captain Planet. 
           Our goal with Operation Regen is to provide a fun, addicting game for children to play and understand that there are simple ways that they can be heroes of the Earth.</p>
